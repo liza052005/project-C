a592d587411d13384a211dd673df56671738206a
liza052005 <lizasmirnova943@gmail.com>
2026-01-30 12:42:43 +1000

    Первая лабораторная работа

--- code/week-1/laba-one.c
+++ code/week-1/laba-one.c
@@ -2,22 +2,20 @@
 #include <math.h>
 
 int main() {
-    double a = 1, b = -5, c = 6;  
-    double D, x1, x2;
+    double a = 1, b = -5, c = 6;
+    double D;
     
-    D = b*b - 4*a*c;
+    D = pow(b, 2) - 4*a*c;  
     
     if (D > 0) {
-        x1 = (-b + sqrt(D)) / (2*a);
-        x2 = (-b - sqrt(D)) / (2*a);
-        printf("Корни: %f и %f\n", x1, x2);
+        printf("x1=%.2f\n", (-b + sqrt(D))/(2*a));
+        printf("x2=%.2f\n", (-b - sqrt(D))/(2*a));
     }
     else if (D == 0) {
-        x1 = -b / (2*a);
-        printf("Корень: %f\n", x1);
+        printf("x=%.2f\n", -b/(2*a));
     }
     else {
-        printf("Корней нет\n");
+        printf("Нет корней\n");
     }
     
     return 0;
