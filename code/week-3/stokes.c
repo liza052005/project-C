13d6a0e80cd8d46e3fcce51cde2f25923f6ff47e
liza052005 <lizasmirnova943@gmail.com>
2026-02-26 13:26:26 +1000

    Продолжение обучения

--- code/week-3/stokes.c
+++ code/week-3/stokes.c
@@ -44,3 +44,33 @@ int main()
     
     return 0;
 }
+#include <stdio.h>
+#include <string.h>  // для strcmp
+
+int main()
+{
+    char password[50] = "secret123";
+    char input[50];
+    int attempts = 3;
+    
+    while(attempts > 0)
+    {
+        printf("Введите пароль (осталось %d попыток): ", attempts);
+        scanf("%s", input);
+        
+        // strcmp возвращает 0 если строки равны
+        if(strcmp(input, password) == 0)
+        {
+            printf("Доступ разрешен!\n");
+            return 0;
+        }
+        else
+        {
+            printf("Неверный пароль!\n");
+            attempts--;
+        }
+    }
+    
+    printf("Доступ заблокирован!\n");
+    return 1;
+}
