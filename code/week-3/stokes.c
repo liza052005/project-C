04951a80674490e55c4ea26cf28035f3eff9277a
liza052005 <lizasmirnova943@gmail.com>
2026-03-04 16:16:26 +1000

    Продолжение обучения

--- code/week-3/stokes.c
+++ code/week-3/stokes.c
@@ -134,3 +134,56 @@ int main()
     
     return 0;
 }
+#include <stdio.h>
+#include <string.h>
+
+void analyze_string(char str[])
+{
+    int length = strlen(str);
+    int words = 1;  // минимум одно слово
+    int letters = 0;
+    int digits = 0;
+    int spaces = 0;
+    int i;
+    
+    for(i = 0; i < length; i++)
+    {
+        if(str[i] == ' ')
+        {
+            spaces++;
+            words++;  // пробел = новое слово
+        }
+        else if((str[i] >= 'a' && str[i] <= 'z') || 
+                (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            letters++;
+        }
+        else if(str[i] >= '0' && str[i] <= '9')
+        {
+            digits++;
+        }
+    }
+    
+    printf("\n=== АНАЛИЗ СТРОКИ ===\n");
+    printf("Строка: %s\n", str);
+    printf("Длина: %d\n", length);
+    printf("Слов: %d\n", words);
+    printf("Букв: %d\n", letters);
+    printf("Цифр: %d\n", digits);
+    printf("Пробелов: %d\n", spaces);
+}
+
+int main()
+{
+    char text[200];
+    
+    printf("Введите строку для анализа: ");
+    fgets(text, sizeof(text), stdin);
+    
+    // Убираем \n
+    text[strlen(text)-1] = '\0';
+    
+    analyze_string(text);
+    
+    return 0;
+}
