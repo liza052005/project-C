2ea9beb75fd75d83a35cb8b22fbb31f37872c74e
liza052005 <lizasmirnova943@gmail.com>
2026-03-19 12:30:24 +1000

    Продолжение обучения

---
+++ code/week-6/practice_4/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "triangle.h"
+
+int main()
+{
+    double a, b, c;
+    
+    printf("Введите стороны треугольника:\n");
+    printf("a = ");
+    scanf("%lf", &a);
+    printf("b = ");
+    scanf("%lf", &b);
+    printf("c = ");
+    scanf("%lf", &c);
+    
+
+    return 0;
+}
