de5edafc7f82d1b8f1bd14805fc1fcf2dffe4acd
liza052005 <lizasmirnova943@gmail.com>
2026-03-19 12:33:35 +1000

    Продолжение обучения

--- code/week-6/practice_4/main.c
+++ code/week-6/practice_4/main.c
@@ -13,6 +13,19 @@ int main()
     printf("c = ");
     scanf("%lf", &c);
     
+    if(a+b>c && a+c>b && b+c>a)
+    {
+        double p = perimetr(a,b,c);
+        double s = area(a,b,c);
+           printf("Периметр: %.2f\n", p);
+        printf("Площадь: %.2f\n", s);
+           printf("Периметр: %.2f\n", p);
+        printf("Площадь: %.2f\n", s);
+    }
+    else
+    {
+        printf("треугольник не существует! \n");
 
+    }
     return 0;
 }
