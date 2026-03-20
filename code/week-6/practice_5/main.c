499ab2e0822aaedeed7dee84280ea15aea99d68e
liza052005 <lizasmirnova943@gmail.com>
2026-03-20 13:00:30 +1000

    продолжение изучения

---
+++ code/week-6/practice_5/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>  // для malloc/free
+#include "matrix_ops.h"
+
+int main()
+{
+    int n;
+    char op;
+    
+    printf("Введите размер матриц n: ");
+    scanf("%d", &n);
+    
+    // дальше будем выделять память
+    
+    return 0;
+}
