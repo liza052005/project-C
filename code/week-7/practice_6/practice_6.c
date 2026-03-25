e18cbd06c262a920833ae2690e49af3f7eba67a0
liza052005 <lizasmirnova943@gmail.com>
2026-03-25 18:35:20 +1000

    Продолжение обучения

---
+++ code/week-7/practice_6/practice_6.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *input, *output;
+    char line[200];
+    
+    input = fopen("people.txt", "r");
+    if(input == NULL)
+    {
+        printf("Ошибка: файл people.txt не найден!\n");
+        return 1;
+    }
+    
+    output = fopen("result.txt", "w");
+    if(output == NULL)
+    {
+        printf("Ошибка создания файла result.txt!\n");
+        fclose(input);
+        return 1;
+    }
+    
+    while(fgets(line, sizeof(line), input) != NULL)
+    {
+        line[strcspn(line, "\n")] = 0;
+        
+        int len = strlen(line);
+        int last_space = -1;
+        
+        for(int i = 0; i < len; i++)
+        {
+            if(line[i] == ' ')
+                last_space = i;
+        }
+        
+        if(last_space != -1)
+        {
+            char year_str[10];
+            strcpy(year_str, &line[last_space + 1]);
+            int year = atoi(year_str);
+            
+            if(year > 1980)
+            {
+                fprintf(output, "%s\n", line);
+            }
+        }
+    }
+    
+    fclose(input);
+    fclose(output);
+    
+    printf("Готово! Люди, родившиеся после 1980 года, сохранены в result.txt\n");
+    
+    return 0;
+}
