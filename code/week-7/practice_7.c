8bc579e4c0e0f955ef258604837bffa48b311c4c
liza052005 <lizasmirnova943@gmail.com>
2026-03-26 11:37:15 +1000

    Продолжение обучения

--- code/week-7/practice_7.c
+++ code/week-7/practice_7.c
@@ -1,15 +1,30 @@
-#include <stdio.h>
-#include <string.h>
+    #include <stdio.h>
+    #include <string.h>
 
-struct humen{
-    char name [50];
-    char surname [50];
-    int year;
+    struct humen{
+        char name [50];
+        char surname [50];
+        int year;
 
-};
-int main()
-{
-    struct humen arr1[4];
-    
-    return 0;
-}
+    };
+    int main()
+    {
+        struct humen arr1[4];
+        struct humen arr2[4];
+        int i;
+        printf("Введите данные о 4 людях:\n");
+        for(i = 0; i < 4; i++)
+        {
+            printf("\n--- Человек %d ---\n", i + 1);
+            
+            printf("Введите имя: ");
+            scanf("%s", arr1[i].name);
+            
+            printf("Введите фамилию: ");
+            scanf("%s", arr1[i].surname);
+            
+            printf("Введите год рождения: ");
+            scanf("%d", &arr1[i].year);
+        }
+        return 0;
+    }
