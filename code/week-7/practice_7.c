5b20630945a65c4213c28c5fda2929b04af9dc35
liza052005 <lizasmirnova943@gmail.com>
2026-03-26 11:36:14 +1000

    Продолжение обучения

--- code/week-7/practice_7.c
+++ code/week-7/practice_7.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+
+struct humen{
+    char name [50];
+    char surname [50];
+    int year;
+
+};
+int main()
+{
+    struct humen arr1[4];
+    
+    return 0;
+}
