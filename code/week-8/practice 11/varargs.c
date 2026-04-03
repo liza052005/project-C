2ed666eaf0c2f9f0ee4ff28570c7b08d3d687b08
liza052005 <lizasmirnova943@gmail.com>
2026-04-03 14:19:39 +1000

    Продолжение обучения

--- code/week-8/practice 11/varargs.c
+++ code/week-8/practice 11/varargs.c
@@ -14,24 +14,41 @@ double sum(int count, ...)
     va_end(args);           
     return total;
 }
-// Максимум чисел
+
 double max(int count, ...)
 {
     va_list args;
     double max_val;
-    
     va_start(args, count);
-    
-    max_val = va_arg(args, double);  // первый аргумент
-    
+    max_val = va_arg(args, double);  
     for(int i = 1; i < count; i++)
     {
         double current = va_arg(args, double);
         if(current > max_val)
             max_val = current;
     }
+    va_end(args);
+    return max_val;
+}
+
+
+double min(int count, ...)
+{
+    va_list args;
+    double min_val;
+    
+    va_start(args, count);
+    
+    min_val = va_arg(args, double);  
+    
+    for(int i = 1; i < count; i++)
+    {
+        double current = va_arg(args, double);
+        if(current < min_val)
+            min_val = current;
+    }
     
     va_end(args);
     
-    return max_val;
+    return min_val;
 }
