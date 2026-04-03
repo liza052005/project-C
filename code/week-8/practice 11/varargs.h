4baaf4a2cc51247134e60cb936cf7c6bf3ef099e
liza052005 <lizasmirnova943@gmail.com>
2026-04-03 14:18:17 +1000

    Продолжение обучения

---
+++ code/week-8/practice 11/varargs.h
@@ -0,0 +1,15 @@
+#ifndef VARARGS_H
+#define VARARGS_H
+
+#include <stdarg.h>  
+
+
+double sum(int count, ...);
+
+double max(int count, ...);
+
+double min(int count, ...);
+
+double average(int count, ...);
+
+#endif
