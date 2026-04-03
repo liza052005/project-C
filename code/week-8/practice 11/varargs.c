d3065df3651b7aba74a441bc7882a85b8fb1b870
liza052005 <lizasmirnova943@gmail.com>
2026-04-03 14:20:07 +1000

    Продолжение обучения

--- code/week-8/practice 11/varargs.c
+++ code/week-8/practice 11/varargs.c
@@ -36,19 +36,28 @@ double min(int count, ...)
 {
     va_list args;
     double min_val;
-    
     va_start(args, count);
-    
     min_val = va_arg(args, double);  
-    
     for(int i = 1; i < count; i++)
     {
         double current = va_arg(args, double);
         if(current < min_val)
             min_val = current;
     }
-    
     va_end(args);
-    
     return min_val;
+}
+
+
+double average(int count, ...)
+{
+    va_list args;
+    double total = 0;
+    va_start(args, count);
+    for(int i = 0; i < count; i++)
+    {
+        total += va_arg(args, double);
+    }
+    va_end(args);
+    return total / count;
 }
