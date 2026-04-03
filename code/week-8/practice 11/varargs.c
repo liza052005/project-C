b77f0880dfbd6e37da7970ff5df6f5b362e6df5c
liza052005 <lizasmirnova943@gmail.com>
2026-04-03 14:17:45 +1000

    Продолжение обучения

--- code/week-8/practice 11/varargs.c
+++ code/week-8/practice 11/varargs.c
@@ -1,18 +1,15 @@
 #ifndef VARARGS_H
 #define VARARGS_H
 
-#include <stdarg.h>  // для va_list, va_start, va_arg, va_end
+#include <stdarg.h> 
 
-// Функция вычисляет сумму чисел
 double sum(int count, ...);
 
-// Функция находит максимум
 double max(int count, ...);
 
-// Функция находит минимум
 double min(int count, ...);
 
-// Функция вычисляет среднее арифметическое
+
 double average(int count, ...);
 
 #endif
