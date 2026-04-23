6fbe09e183d0688f06cb202a667bd6b00565495c
liza052005 <lizasmirnova943@gmail.com>
2026-04-23 11:16:24 +1000

    фунцкия поиска зданий по этажности

--- code/week-11/task_func.c
+++ code/week-11/task_func.c
@@ -16,3 +16,27 @@ int main() {
     }
     return 0;
 }
+#include <stdio.h>
+
+int findBuilding(int arr[], int n, int value) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int buildings[5] = {4, 7, 2, 9, 5};
+    int search = 9;
+    
+    int index = findBuilding(buildings, 5, search);
+    
+    if (index != -1) {
+        printf("Здание с %d этажами найдено на позиции %d\n", search, index);
+    } else {
+        printf("Здание с %d этажами не найдено\n", search);
+    }
+    return 0;
+}
