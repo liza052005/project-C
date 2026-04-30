ce4accf7f0cc479d494d1efd1a84c88529730c48
liza052005 <lizasmirnova943@gmail.com>
2026-04-30 18:44:21 +1000

    Создание функции пузырьковой сортировки

--- code/week-13/task_sort.c
+++ code/week-13/task_sort.c
@@ -4,4 +4,17 @@ void printArray(int arr[], int size) {
         printf("%d ", arr[i]);
     }
     printf("\n");
+
+void bubbleSortDesc(int arr[], int size) {
+    int temp;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            // Если текущий элемент МЕНЬШЕ следующего — меняем (для убывания)
+            if (arr[j] < arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
