6591b48115dd068255c7cbf76c7ae26d704a28ac
liza052005 <lizasmirnova943@gmail.com>
2026-04-30 18:46:29 +1000

    Подсчет отличников + сортировка

--- code/week-13/task_sort.c
+++ code/week-13/task_sort.c
@@ -30,20 +30,16 @@ int countExcellent(int arr[], int size) {
 int main() {
     int grades[] = {3, 5, 2, 4, 5, 3, 4, 5, 2, 4};
     int size = sizeof(grades) / sizeof(grades[0]);
-
     printf("Исходный массив оценок: ");
     printArray(grades, size);
 
     // Подсчёт отличников в исходном массиве
     int excellentCount = countExcellent(grades, size);
     printf("Количество отличников в исходном массиве: %d\n", excellentCount);
-
     // Сортировка
     bubbleSortDesc(grades, size);
-
     printf("Отсортированный массив (по убыванию): ");
     printArray(grades, size);
-
     // Проверка, что количество отличников не изменилось
     excellentCount = countExcellent(grades, size);
     printf("Количество отличников после сортировки: %d\n", excellentCount);
