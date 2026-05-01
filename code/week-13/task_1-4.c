12550357e9d8488332da6ba5cd66a92a4ad8f229
liza052005 <lizasmirnova943@gmail.com>
2026-05-01 13:22:53 +1000

    Выполнение практических заданий для зачета

---
+++ code/week-13/task_1-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    double a, b, c, x1, x2;
+    printf("Введите коэф-ты:: \n");
+    scanf("%lf %lf %lf, &a, &b, &c");
+    d = pow(b,2) - 4*a*c;
+    
+}
