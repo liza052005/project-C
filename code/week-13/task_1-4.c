029d4cf1c146ecdd651a9bc78150d8bdc6d4da80
liza052005 <lizasmirnova943@gmail.com>
2026-05-01 13:51:10 +1000

    Заполнение дневника

--- code/week-13/task_1-4.c
+++ code/week-13/task_1-4.c
@@ -1,22 +1,22 @@
-// #include <stdio.h>
-// #include <math.h>
+#include <stdio.h>
+#include <math.h>
 
-// void main()
-// {
-//     double a, b, c, x1, x2;
-//     printf("Введите коэф-ты:: \n");
-//     scanf("%lf %lf %lf, &a, &b, &c");
-//     d = pow(b,2) - 4*a*c;
-//     if(d<0)
-//     {
-//         x1 = (-b + sqrt(d))/(2*a);
-//         x2 = (-b - sqrt(d))/(2*a);
-//     }
-//     else
-//     {
-//         printf("Нет корней\n");
-//     }
-// }
+void main()
+{
+    double a, b, c, x1, x2;
+    printf("Введите коэф-ты:: \n");
+    scanf("%lf %lf %lf, &a, &b, &c");
+    d = pow(b,2) - 4*a*c;
+    if(d<0)
+    {
+        x1 = (-b + sqrt(d))/(2*a);
+        x2 = (-b - sqrt(d))/(2*a);
+    }
+    else
+    {
+        printf("Нет корней\n");
+    }
+}
 
 // таск 2
 #include <stdio.h>
