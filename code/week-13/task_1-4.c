90146c4e2bfd32bdd65130ed2c499949eb8c03bf
liza052005 <lizasmirnova943@gmail.com>
2026-05-01 13:24:28 +1000

    Первая практика - находим корни уравнения

--- code/week-13/task_1-4.c
+++ code/week-13/task_1-4.c
@@ -7,5 +7,9 @@ void main()
     printf("Введите коэф-ты:: \n");
     scanf("%lf %lf %lf, &a, &b, &c");
     d = pow(b,2) - 4*a*c;
-    
+    if(d<0)
+    {
+        x1 = (-b + sqrt(d))/(2*a);
+        x2 = (-b - sqrt(d))/(2*a);
+    }
 }
