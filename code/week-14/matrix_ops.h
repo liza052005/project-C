37bca5184773587846151f6fc493207c4804cc94
liza052005 <lizasmirnova943@gmail.com>
2026-05-07 10:41:44 +1000

    Пятое задание - создание файла с объявлением функций: сложение, уножение, деление

---
+++ code/week-14/matrix_ops.h
@@ -0,0 +1,6 @@
+#ifndef MATRIX_OPS_H
+#define MATRIX_OPS_H
+double** matrix_add(double **a, double **b, int n);
+double** matrix_sub(double **a, double **b, int n);
+double** matrix_mul(double **a, double **b, int n);
+#endif
