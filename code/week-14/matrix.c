5ce8539626ce405754819c3f4003a22fc11557ac
liza052005 <lizasmirnova943@gmail.com>
2026-05-07 10:45:28 +1000

    Создание функции умножения. Числа перемножаются парами

--- code/week-14/matrix.c
+++ code/week-14/matrix.c
@@ -28,3 +28,22 @@ double** matrix_sub(double **a, double **b, int n)
     }
     return res;
 }
+double** matrix_mul(double **a, double **b, int n)
+{
+    int i, j, k;
+    double **res = (double**)malloc(n * sizeof(double*));
+    
+    for (i = 0; i < n; i++)
+    {
+        res[i] = (double*)malloc(n * sizeof(double));
+        for (j = 0; j < n; j++)
+        {
+            res[i][j] = 0;
+            for (k = 0; k < n; k++)
+            {
+                res[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    return res;
+}
